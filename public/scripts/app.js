092b9e31f972f959c784b29fb98a79e62f34a5ed
Olajumoke Oladimeji <jumoke5ng@yahoo.com>
2015-09-16 10:32:49 +0100

    styling the single services page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,25 +14,35 @@ var app = angular.module('DeveloperCentral', [
     'ngSanitize',
     'ngTouch',
     'ui.bootstrap'
-  ]);
-app.config(function ($routeProvider) {
+]);
+app.config(function($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/airtime', {
-        templateUrl: 'views/airtime-purchase.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/services', {
-        templateUrl: 'views/services-list.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/services', {
+            templateUrl: 'views/services-list.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/airtime-purchases', {
+            templateUrl: 'views/airtime-purchase.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/transfers', {
+            templateUrl: 'views/transfer.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/payments', {
+            templateUrl: 'views/payments.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
