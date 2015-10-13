cf69e3e1f7e6c5541051c3a4cc78ca3354682674
Olajumoke Oladimeji <jumoke5ng@yahoo.com>
2015-10-13 09:45:20 +0100

    Updated the user and app model for referencing the app created by the user in the user model

--- app/models/user.js
+++ app/models/user.js
@@ -62,9 +62,29 @@ var UserSchema = new Schema({
     type: Boolean,
     default: false
   },
+  billPaymentCount: {
+    type: Number,
+    default: 0
+  },
+  rechargesCount: {
+    type: Number,
+    default: 0
+  },
   apps: [{
     type: Schema.ObjectId,
     ref: 'App'
+  }],
+  keys:[{
+    type: Schema.ObjectId,
+    ref: 'ApiKey'
+  }],
+  billPayments: [{
+    type: Schema.ObjectId,
+    ref: 'BillsPayment'
+  }],
+  recharges: [{
+    type: Schema.ObjectId,
+    ref: 'AirtimeRecharge'
   }],
   createdAt: {
     type: Date,
